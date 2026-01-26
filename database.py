6a0e4506113fd1f33ef09498cdc143acdcc15f24
visionof2030-pixel <visionof2030@gmail.com>
2026-01-27 01:15:24 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,11 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
 def init_db():
     conn = get_connection()
     cur = conn.cursor()

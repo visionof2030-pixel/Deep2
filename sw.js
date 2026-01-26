3dead1c4ea36bd53f4e91c78e853e5fd478edfeb
visionof2030-pixel <visionof2030@gmail.com>
2026-01-26 18:59:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,2 @@
-// sw.js
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("admin-cache").then(c =>
-      c.addAll([
-        "/admin.html",
-        "/manifest.json"
-      ])
-    )
-  )
-})
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  )
-})
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("fetch", e => {});

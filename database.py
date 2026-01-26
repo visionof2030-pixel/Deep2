2c6d0eadbcabac8d6b66251327885380619457d5
visionof2030-pixel <visionof2030@gmail.com>
2026-01-26 23:18:59 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    if not DATABASE_URL:
-        raise Exception("DATABASE_URL not set")
     return psycopg2.connect(DATABASE_URL)
 
 def init_db():

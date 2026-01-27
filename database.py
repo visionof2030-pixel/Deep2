3728f8c607f26d074964bc61225b3aa9e8a91085
visionof2030-pixel <visionof2030@gmail.com>
2026-01-28 02:01:43 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,20 +4,23 @@ import psycopg2
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode="require")
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL not set")
+    return psycopg2.connect(DATABASE_URL)
 
 def init_db():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS activation_codes (
-        id SERIAL PRIMARY KEY,
-        code TEXT UNIQUE NOT NULL,
-        is_active BOOLEAN DEFAULT TRUE,
-        expires_at TIMESTAMP,
-        usage_limit INTEGER,
-        usage_count INTEGER DEFAULT 0
-    );
+        CREATE TABLE IF NOT EXISTS activation_codes (
+            id SERIAL PRIMARY KEY,
+            code TEXT UNIQUE NOT NULL,
+            name TEXT,
+            is_active BOOLEAN DEFAULT TRUE,
+            expires_at TIMESTAMP NULL,
+            usage_limit INTEGER NULL,
+            usage_count INTEGER DEFAULT 0
+        )
     """)
     conn.commit()
     conn.close()

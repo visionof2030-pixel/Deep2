ffe9a6654c29368d96bddd83e6049210fab2402f
visionof2030-pixel <visionof2030@gmail.com>
2026-01-28 04:05:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
+import itertools
 import google.generativeai as genai
+from database import init_db
+import traceback
 
 app = FastAPI()
+init_db()
 
-class Req(BaseModel):
+class AskReq(BaseModel):
     prompt: str
+
+api_keys = [os.getenv(f"GEMINI_API_KEY_{i}") for i in range(1, 8)]
+api_keys = [k for k in api_keys if k]
+key_cycle = itertools.cycle(api_keys)
+
+def get_api_key():
+    return next(key_cycle)
 
 @app.get("/")
 def root():
     return {"status": "server is running"}
 
 @app.post("/ask")
-def ask(req: Req):
-    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    model = genai.GenerativeModel("models/gemini-2.5-flash-lite")
-    response = model.generate_content(req.prompt)
-    return {"answer": response.text}
+def ask(req: AskReq):
+    try:
+        genai.configure(api_key=get_api_key())
+        model = genai.GenerativeModel("models/gemini-2.5-flash-lite")
+        r = model.generate_content(req.prompt)
+        return {"answer": r.text}
+
+    except Exception as e:
+        return {
+            "error": "Gemini failed",
+            "detail": str(e)
+        }

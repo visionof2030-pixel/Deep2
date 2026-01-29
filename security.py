82372bed1f1a0d16859bfcfdc10ea5973cf9ca64
visionof2030-pixel <visionof2030@gmail.com>
2026-01-29 04:28:55 +0300

    Update security.py

--- security.py
+++ security.py
@@ -1,6 +1,11 @@
 # security.py
-from fastapi import Header, Depends
+from fastapi import Header, HTTPException
 from key_logic import verify_code
 
-def activation_required(x_activation_code: str = Header(...)):
+def activation_required(
+    x_activation_code: str = Header(..., alias="X-Activation-Code")
+):
+    if not x_activation_code:
+        raise HTTPException(status_code=401, detail="Activation code missing")
+
     verify_code(x_activation_code)

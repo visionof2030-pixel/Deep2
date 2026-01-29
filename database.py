8d378f770bf4f14ab47034277d3f8beb32ea0611
visionof2030-pixel <visionof2030@gmail.com>
2026-01-29 05:57:47 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,7 @@
-# database.py
 import os
 import psycopg2
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL not set")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
@@ -16,9 +12,9 @@ def init_db():
     cur.execute("""
     CREATE TABLE IF NOT EXISTS activation_codes (
         id SERIAL PRIMARY KEY,
-        code TEXT UNIQUE,
+        code TEXT UNIQUE NOT NULL,
         is_active BOOLEAN DEFAULT TRUE,
-        created_at TIMESTAMP,
+        created_at TIMESTAMP NOT NULL,
         expires_at TIMESTAMP,
         usage_limit INTEGER,
         usage_count INTEGER DEFAULT 0,

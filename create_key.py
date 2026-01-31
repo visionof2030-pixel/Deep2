9b1045e328d3f4fb1d03e009e248ce7d4e2f4eea
visionof2030-pixel <visionof2030@gmail.com>
2026-01-31 22:16:32 +0300

    Update create_key.py

--- create_key.py
+++ create_key.py
@@ -1,23 +1,25 @@
-# create_key.py
 import uuid
 from datetime import datetime
 from database import get_connection
 
 def create_key(expires_at=None, usage_limit=None):
     code = str(uuid.uuid4())
-
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("""
+    cur.execute(
+        """
         INSERT INTO activation_codes
         (code, created_at, expires_at, usage_limit)
         VALUES (%s, %s, %s, %s)
-    """, (
-        code,
-        datetime.utcnow(),
-        expires_at,
-        usage_limit
-    ))
+        """,
+        (
+            code,
+            datetime.utcnow(),
+            expires_at,
+            usage_limit
+        )
+    )
     conn.commit()
+    cur.close()
     conn.close()
     return code
